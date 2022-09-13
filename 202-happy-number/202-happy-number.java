c1d62066baee2fdf34b84875b1900986792bd77e
Vlad Berezan <vberezan@users.noreply.github.com>
2022-09-13 14:10:56 +0300

    Time: 2 ms (67.81%), Space: 40.5 MB (83.79%) - LeetHub

---
+++ 202-happy-number/202-happy-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = next(n);
+        
+        while (fast > 1 && slow != fast) {
+            slow = next(slow);
+            fast = next(next(fast));
+        }
+        
+        return fast == 1;
+    }
+    
+    private static int next(int n) {
+        int result = 0;
+        
+        while (n > 0) {
+            int last = n % 10;
+            n = n / 10;
+            
+            result += Math.pow(last, 2);
+        }
+        
+        return result;
+    }
+}
