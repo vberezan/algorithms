aa5f20ffdc9c4f4eee030f1066f7615facca7370
Vlad Berezan <vberezan@users.noreply.github.com>
2022-11-14 20:47:09 +0200

    Time: 5 ms (80.62%), Space: 45.9 MB (23.02%) - LeetHub

--- 1-two-sum/solution--default.java
+++ 1-two-sum/solution--default.java
@@ -3,6 +3,7 @@ class Solution {
     private final HashMap<Integer, Integer> pairs = new HashMap<>();
 
     public int[] twoSum(int[] nums, int target) {
+        
         for (int i = 0; i < nums.length; i++) {
             int num = nums[i];
 
